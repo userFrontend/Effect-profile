5a8f49f5dff39b1a40dbe3373a102975accc0652
doston <raximoffdoston@gmail.com>
2024-10-20 17:36:20 +0500

    save

---
+++ src/components/Phones/Phones.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./Phones.scss"
+
+const Phones = () => {
+  return (
+    <div className='container'>
+        <h1>
+            Hello world
+        </h1>
+    </div>
+  )
+}
+
+export default Phones
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,48 +4,19 @@ gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
 import './Home.scss'
 import PhotoGallery from '../../components/PhotoGallery/PhotoGallery';
 import Hero from '../../components/Hero/Hero';
+import Phones from '../../components/Phones/Phones';
 
 const Home = () => {
 
   return (
     <main>
-        <div>
+    <div>
       <Hero />
       <section className="scroll-section">
         <PhotoGallery />
       </section>
     </div>
-        {/* <section className="wrapper">
-                <div className="content">
-                    
-
-                    <div className="portfolio">
-                    <div className="container">
-                        <main className="gallery">
-                        <div data-speed=".9" className="gallery__left">
-                            <img className="gallery__item" src="/images/heroso.png" alt="Alt" />
-                            <img className="gallery__item" src="/images/heroso.png" alt="Alt" />
-                            <div className="text-block gallery__item">
-                            <h2 className="text-block__h">Creative floating scroll with amazing parallax effect</h2>
-                            <p className="text-block__p">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor amount scrolling.</p>
-                            </div>
-                            <img className="gallery__item" src="/images/heroso.png" alt="Alt" />
-                        </div>
-
-                        <div data-speed="1.1" className="gallery__right">
-                            <div className="text-block gallery__item">
-                            <h2 className="text-block__h">Creative floating scroll with amazing parallax effect</h2>
-                            <p className="text-block__p">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor amount scrolling.</p>
-                            </div>
-                            <img className="gallery__item" src="/images/heroso.png" alt="Alt" />
-                            <img className="gallery__item" src="/images/heroso.png" alt="Alt" />
-                            <img className="gallery__item" src="/images/heroso.png" alt="Alt" />
-                        </div>
-                        </main>
-                    </div>
-                    </div>
-                </div>
-        </section> */}
+    <Phones />
     </main>
   );
 }
