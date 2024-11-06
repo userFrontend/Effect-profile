3e1f86611bbf449632021b78883fd44ca5c1c3a3
userFrontend <aba06096@gmail.com>
2024-11-06 21:17:18 +0500

    save

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import './Header.scss'
 import { Link, NavLink } from "react-router-dom";
 import { useInfoContext } from "../../context/infoContext";
 import { MdOutlineLightMode } from "react-icons/md";
 import { BsMoonStars } from "react-icons/bs";
 import { LuSun } from "react-icons/lu";
+import { FaRegSun } from "react-icons/fa";
 
 const Header = () => {
-  const {theme, toggleTheme} = useInfoContext()
+  const {handleLanguageChange, theme, toggleTheme} = useInfoContext()
+    const [openlang, setOpenlang] = useState(false);
+  
+    // Toggle dropdown on click
+    const toggle = () => setOpenlang(!openlang);
+
 
   return (
       <header className={theme === 'dark' ? "header" : "header-dark"}>
@@ -19,18 +25,42 @@ const Header = () => {
                         </Link>
                     <h2>EFFECT</h2>
                 </div>
-                <label className="switch">
-                  <input checked={theme === 'dark'} type="checkbox"  className="input__check" onChange={toggleTheme}/>
-                  <span className="slider"></span>
-                </label>
-                {/* <ul className="list">
-                    <li to='/' className="items">
-                    <input checked={theme === 'dark'} id="checkbox" type="checkbox" onChange={toggleTheme}/>
-                    <label className="switch" htmlFor="checkbox">
-                      {theme === 'dark' ? <LuSun /> : <BsMoonStars />}
-                    </label>
-                    </li>
-                </ul> */}
+                <div className="options">
+                <div className="change">
+                  <div className="lang">
+                    <div onClick={toggle} className="lang-img">
+                      <img src={`/images/${localStorage.getItem('language') || 'ru'}.svg`} alt='language' />
+                    </div>
+                  </div>
+                  {openlang && (
+                    <div className="box-lang">
+                        <div className='lang' onClick={() => {toggle(); handleLanguageChange('en')}}>
+                          <div className="lang-img">
+                            <img src={`/images/en.svg`} alt="English" />
+                          </div>
+                          <b>English</b>
+                        </div>
+                        <div className='lang' onClick={() => {toggle(); handleLanguageChange('ru')}}>
+                          <div className="lang-img">
+                            <img src={`/images/ru.svg`} alt="Russian" />
+                          </div>
+                          <b>Русскый</b>
+                        </div>
+                        <div className='lang' onClick={() => {toggle(); handleLanguageChange('uz')}}>
+                          <div className="lang-img">
+                            <img src={`/images/uz.svg`} alt="O'zbek" />
+                          </div>
+                          <b>O'zbek</b>
+                        </div>
+                    </div>
+                  )}
+                </div>
+                  <input checked={theme === 'dark'} onChange={toggleTheme} type="checkbox" id="checkbox" />
+                  <label htmlFor="checkbox" className="switch">
+                    {theme === 'dark' ? <FaRegSun /> : <BsMoonStars />}
+                    {/* {theme === 'dark' ? "Dark" : "Light"} */}
+                  </label>
+                </div>                
             </nav> 
         </div>
       </header>
